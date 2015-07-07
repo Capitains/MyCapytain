9ce44b668a494bfe782c7e0821bcb0d7c9066119
Thibault Clérice <leponteineptique@gmail.com>
2015-07-07 13:17:45 +0200

    PEP8

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import setup
 
-setup(name='MyCapytain',
-      version='0.1.0',
-      description='Abstraction of CTS API for Python',
-      url='http://github.com/Capitains/MyCapytain',
-      author='Thibault Clerice',
-      author_email='leponteineptique@gmail.com',
-      license='MIT',
-      packages=['MyCapytain'],
-      install_requires=[
-        "requests==2.7.0",
-        "six==1.9.0",
-        "lxml==3.4.4"
-      ],
-      tests_require=[
-        "mock==1.0.1",
-        "xmlunittest"
-      ],
-      extras_require = {
-        "DOC" : ["Sphinx==1.3.1"]
-      },
-      test_suite="tests",
-      zip_safe=False)
+setup(
+  name='MyCapytain',
+  version='0.1.0',
+  description='Abstraction of CTS API for Python',
+  url='http://github.com/Capitains/MyCapytain',
+  author='Thibault Clerice',
+  author_email='leponteineptique@gmail.com',
+  license='MIT',
+  packages=['MyCapytain'],
+  install_requires=[
+    "requests==2.7.0",
+    "six==1.9.0",
+    "lxml==3.4.4"
+  ],
+  tests_require=[
+    "mock==1.0.1",
+    "xmlunittest"
+  ],
+  extras_require = {
+    "DOC" : ["Sphinx==1.3.1"]
+  },
+  test_suite="tests",
+  zip_safe=False
+)

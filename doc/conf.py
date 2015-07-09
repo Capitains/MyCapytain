9102929a8aa5cc4a94a37368d8eeb6c9624232c0
Thibault Clérice <leponteineptique@gmail.com>
2015-07-09 18:31:08 +0200

    Another unicode

--- doc/conf.py
+++ doc/conf.py
@@ -231,7 +231,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'MyCapytains.tex', 'MyCapytains Documentation',
-   'Thibault Clérice', 'manual'),
+   u'Thibault Clérice', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
